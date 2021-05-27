eeebc4b1353436690c6026e144f53d87e07a4c81
Bruno Leão <leaodebrito@icloud.com>
2021-05-27 18:33:29 -0300

    Initial Commit

---
+++ LoginPage/LoginPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginPageApp.swift
+//  LoginPage
+//
+//  Created by Bruno Brito on 27/05/21.
+//
+
+import SwiftUI
+
+@main
+struct LoginPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
